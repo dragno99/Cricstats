ae2b895cc071f880ece059c7ede50e4633c77331
Suryansh Kumar <76701875+dragno99@users.noreply.github.com>
2021-06-18 06:27:12 +0530

    Add files via upload

---
+++ Cricstats/delete_stadium.php
@@ -0,0 +1,21 @@
+<?php
+
+    session_start();
+
+    $con = mysqli_connect("localhost", "root", "password", "cricket") or die(mysqli_error($con));
+
+    $stadium_name=$_POST['stadium_name'];
+    $sql="DELETE from stadiums where stadium_name='$stadium_name'";
+    if(mysqli_query($con,$sql))
+    {
+
+        echo "<script type='text/javascript'>alert('Stadium deleted successfully!!');</script>";
+      header("refresh: 0.01; url=deletestadium.html");
+
+}
+else{
+    echo "error";
+    header("refresh: 0.01; url=deletestadium.html");
+
+}
+?>
